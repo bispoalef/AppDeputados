49de1dd305c5c550923f1a319cd5b51316baf901
bispoalef <alef.b.dos.santos.1993@gmail.com>
2022-08-30 22:51:58 -0300

    testes nas requisições http

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 import 'package:trste_api/parlamentares.dart';
 
 class ApiData extends ChangeNotifier {
@@ -8,6 +9,7 @@ class ApiData extends ChangeNotifier {
 
   carregarDados() async {
     await _buscarDados();
+    await _buscarGastos();
   }
 
   _buscarDados() async {
@@ -16,5 +18,19 @@ class ApiData extends ChangeNotifier {
     var deputados = data['deputados']['deputado'];
 
     listaDeputados = deputados;
+  }
+
+  _buscarGastos() async {
+    var url = Uri.parse(
+        'https://dadosabertos.camara.leg.br/api/v2/deputados/74459/despesas?ordem=ASC&ordenarPor=ano');
+    var response = await http.get(url);
+
+    final jsonResponse = jsonDecode(response.body);
+
+    print(jsonResponse['dados'][3]['valorDocumento']);
+
+    // var deputados = data['deputados']['deputado'];
+
+    // listaDeputados = deputados;
   }
 }
